2d093d44953f626e1b4c2be6db950d8a15231c1a
초록꼬마 <greenkey20@gmail.com>
2021-11-08 08:13:56 +0900

    2021.10.29자 상속 과제2 v2_다시 해 봄(2021.11.6)

---
+++ 00_Exercises/src/com/kh/practice/pointv2/run/Run.java
@@ -0,0 +1,13 @@
+package com.kh.practice.pointv2.run;
+
+import com.kh.practice.pointv2.view.PointMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		new PointMenu().mainMenu(); // 굳이 별명 만들지 않음
+
+	}
+
+}

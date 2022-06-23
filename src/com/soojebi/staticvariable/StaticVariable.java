c671e2fbbd0073735212858084f0971d1f778ef5
초록꼬마 <greenkey20@gmail.com>
2022-06-23 20:56:14 +0900

    feat: add the exercises from Soojebi

---
+++ src/com/soojebi/staticvariable/StaticVariable.java
@@ -0,0 +1,11 @@
+package com.soojebi.staticvariable;
+
+/**
+ * @author greenpianorabbit
+ * 2022.6.23(목) 20h50 정보처리기사 실기 프로그래밍 언어 활용
+ */
+public class StaticVariable {
+	
+	static int count = 0;
+
+}
---
+++ src/com/soojebi/staticvariable/StaticVariableMain.java
@@ -0,0 +1,19 @@
+package com.soojebi.staticvariable;
+
+/**
+ * @author greenpianorabbit
+ * 2022.6.23(목) 20h50 정보처리기사 실기 프로그래밍 언어 활용
+ */
+public class StaticVariableMain {
+
+	public static void main(String[] args) {
+		StaticVariable sv = new StaticVariable();
+		
+		sv.count++;
+		System.out.println(sv.count); // 1
+		
+		sv.count++;
+		System.out.println(sv.count); // 2
+	}
+
+}

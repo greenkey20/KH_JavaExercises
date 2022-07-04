1faa62c61e2f4f6bedcfe0ca9debc2f6d9f0e7f7
초록꼬마 <greenkey20@gmail.com>
2022-07-04 23:59:20 +0900

    feat: add shift operators

---
+++ 00_Exercises/src/com/soojebi/shiftoperator/ShiftOperator.java
@@ -0,0 +1,15 @@
+package com.soojebi.shiftoperator;
+
+/**
+ * @author greenpianorabbit
+ * 2022.7.4(월) 23h45 정보처리기사 실기 프로그래밍 언어 활용 6-34
+ * shift 연산자 = 피연산자의 2진수 표현의 각 자리를 오른쪽(>>) 또는 왼쪽(<<)으로 이동(shift)
+ */
+public class ShiftOperator {
+
+	public static void main(String[] args) {
+		System.out.println(8 >> 3); // 1
+		System.out.println(3 << 2); // 12
+	}
+
+}
